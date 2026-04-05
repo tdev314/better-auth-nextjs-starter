9dd954eeec4e66cec32fda1c35ae5070f2da61d2
tai <15724094+tai-314@users.noreply.github.com>
2026-04-05 12:03:50 -0400

    Add new migration entry for version 7 in _journal.json to track changes related to 'great_boom_boom'.

---
+++ migrations/0001_great_boom_boom.sql
@@ -0,0 +1,3 @@
+CREATE INDEX "accounts_userId_idx" ON "accounts" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "sessions_userId_idx" ON "sessions" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "verifications_identifier_idx" ON "verifications" USING btree ("identifier");
