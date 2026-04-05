4f20b197dd44f1e32e01f1686db97124a6744eef
tai <15724094+tai-314@users.noreply.github.com>
2026-04-05 09:50:24 -0400

    Enhance auth schema with default values and update timestamps
    Updated the auth schema to include default values for email verification and timestamps in the users, sessions, accounts, and verifications tables. This ensures better data integrity and automatic handling of created and updated timestamps. Additionally, corrected the schema path in drizzle.config.ts to point to the new location.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
     out: "./migrations",
-    schema: "./database/schema.ts",
+    schema: "./src/database/schema.ts",
     dialect: "postgresql",
     dbCredentials: {
         url: process.env.DATABASE_URL!
