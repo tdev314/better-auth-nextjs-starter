f230c33cad0002ff76c09eba9cd86d5315fd3569
tai <15724094+tai-314@users.noreply.github.com>
2026-04-05 11:45:50 -0400

    Redirect Home component to sign-in page for improved authentication flow.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation"
+
 export default function Home() {
-    return (
-        <main className="container mx-auto flex flex-col gap-4 p-6">
-            {/* <h1 className="font-bold text-2xl">Hello, world.</h1> */}
-        </main>
-    )
+    redirect("/auth/sign-in")
 }
